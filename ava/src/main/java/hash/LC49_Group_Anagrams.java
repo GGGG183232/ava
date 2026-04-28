9e90362a42fed87bf1059d104fb627043a63f334
GGGG183232 <2608224156@qq.com>
2026-04-28 12:02:24 +0800

    add

--- ava/src/main/java/hash/LC49_Group_Anagrams.java
+++ ava/src/main/java/hash/LC49_Group_Anagrams.java
@@ -2,11 +2,26 @@
 
 package hash;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class LC49_Group_Anagrams {
     public static void main(String arg[])
     {
         String arr[] = {"eat","tea","tan","ate","nat","bat"};
-        
+        HashMap<String,List<String>> map = new HashMap<String, List<String>>();
+        for(String str :arr)
+        {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
 
+        }
+        for(String str)
+        {
+            System.out.println();
+        }
     }
 }
